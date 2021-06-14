ebe3827d4d6195867ac992a718434af77ca15d6e
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-06-14 10:04:12 -0300

    fix: parameter name

--- src/python/main.py
+++ src/python/main.py
@@ -5,4 +5,4 @@ VIOLIN_MAX_F=librosa.note_to_hz('E7')
 
 y, sr = librosa.load('/home/rafa/sci/sound/0/0.wav')
 f0, voiced_flag, voiced_probs = \
-    librosa.pyin(y, fmin=VIOLIN_MIN_F, fmin=VIOLIN_MAX_F)
+    librosa.pyin(y, fmin=VIOLIN_MIN_F, fmax=VIOLIN_MAX_F)
