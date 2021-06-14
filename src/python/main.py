4dc59c19f245b03739c39e2afc211da710f66752
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-06-14 10:12:38 -0300

    add function to extract pitch from sound file

--- src/python/main.py
+++ src/python/main.py
@@ -3,6 +3,13 @@ import librosa
 VIOLIN_MIN_F=librosa.note_to_hz('G3')
 VIOLIN_MAX_F=librosa.note_to_hz('E7')
 
-y, sr = librosa.load('/home/rafa/sci/sound/0/0.wav')
-f0, voiced_flag, voiced_probs = \
-    librosa.pyin(y, fmin=VIOLIN_MIN_F, fmax=VIOLIN_MAX_F)
+def get_pitch(snd_filename, fmin=VIOLIN_MIN_F, fmax=VIOLIN_MAX_F):
+    """Extract f0 from sound file."""
+    y, sr = librosa.load(snd_filename)
+    f0, voiced_flag, voiced_probs = \
+        librosa.pyin(y, fmin=VIOLIN_MIN_F, fmax=VIOLIN_MAX_F)
+
+    return f0
+
+pitch = get_pitch('/home/rafa/sci/sound/440/440.wav')
+pass
