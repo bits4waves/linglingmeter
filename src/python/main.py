8661e59228a39753f947f1e4d78e5dc6a5adf05b
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-06-14 09:47:11 -0300

    use pYIN from librosa for pitch detection

---
+++ src/python/main.py
@@ -0,0 +1,8 @@
+import librosa
+
+VIOLIN_MIN_F=librosa.note_to_hz('G3')
+VIOLIN_MAX_F=librosa.note_to_hz('C8')
+
+y, sr = librosa.load('/home/rafa/sci/sound/0/0.wav')
+f0, voiced_flag, voiced_probs = \
+    librosa.pyin(y, fmin=VIOLIN_MIN_F, fmin=VIOLIN_MAX_F)
