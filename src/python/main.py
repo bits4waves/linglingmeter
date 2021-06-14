aaaf653f8f35a0ebd140eef1304bb69c0c8fbbd7
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-06-14 10:03:56 -0300

    use a more conservative max freq

--- src/python/main.py
+++ src/python/main.py
@@ -1,7 +1,7 @@
 import librosa
 
 VIOLIN_MIN_F=librosa.note_to_hz('G3')
-VIOLIN_MAX_F=librosa.note_to_hz('C8')
+VIOLIN_MAX_F=librosa.note_to_hz('E7')
 
 y, sr = librosa.load('/home/rafa/sci/sound/0/0.wav')
 f0, voiced_flag, voiced_probs = \
