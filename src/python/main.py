ded5dda442a194ca2557254a7254da6a7a002c60
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-06-15 10:00:53 -0300

    plot the spectrogram with f0 overlayed

--- src/python/main.py
+++ src/python/main.py
@@ -13,7 +13,39 @@ def get_pitch_series(snd_filename, fmin=VIOLIN_MIN_F,
     f0, voiced_flag, voiced_probs = \
         librosa.pyin(y, fmin=VIOLIN_MIN_F, fmax=VIOLIN_MAX_F)
 
-    return f0
+    return y, f0
 
-pitch = get_pitch_series('/home/rafa/sci/sound/440/440.wav')
-pass
+
+y, pitch = get_pitch_series('/home/rafa/sci/sound/440-02-partials/440-02-partials.wav')
+
+# Overlay F0 over a spectrogram
+
+import matplotlib.pyplot as plt
+import numpy as np
+import librosa.display
+
+amplitude = np.abs(librosa.stft(y))
+dB = librosa.amplitude_to_db(amplitude, ref=np.max)
+
+fig, ax = plt.subplots()
+img = librosa.display.specshow(dB, x_axis='time', y_axis='log', ax=ax)
+ax.set(title='pYIN fundamental frequency estimation')
+fig.colorbar(img, ax=ax, format="%+2.f dB")
+times = librosa.times_like(pitch)
+ax.plot(times, pitch*FIFTY_CENTS_BWD, label='bwd', color='red', linewidth=1)
+ax.plot(times, pitch, label='f0', color='cyan', linewidth=1)
+ax.plot(times, pitch*FIFTY_CENTS_FWD, label='fwd', color='red', linewidth=1)
+ax.legend(loc='upper right')
+fig.savefig('plot.png')
+
+# for each entry in the array
+# find peaks
+# extract pitch
+
+# for i = 1, 2, …, len(peaks)
+#     partial = pitch * i
+#     for peak in peaks
+#         if abs((partial - peak) / partial) ≤ threshold
+#             integral_peaks += integrate_peak(peak)
+
+# score = integral_peaks / integrate_tail(pitch)
