ee475ec458a8bc0f1c68fc559127e63e85e052cb
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-06-25 08:55:14 -0300

    experiment with xpath-based scraping

--- src/html/get_it.sh
+++ src/html/get_it.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# /html/body/div/div/div[2]/div[2]/div[5]/div[1]/div/div/div[2]/div[2]
+# /html/body/div/div/div[2]/div[2]/div[5]/div[1]/div/div/div[635]/div[2]
 xmllint --xpath '//div[contains(concat(" ", @class, " "), " c02 ")]' catgut-papers.html > catgut-papers.txt
 
