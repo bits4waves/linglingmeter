c5e5df4bcc89506f29b5b5f276da6fbc0f857c32
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-06-29 08:48:52 -0300

    add python scraper script

---
+++ src/python/scraper.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class ArticlesSpider(scrapy.Spider):
+    name = 'articles'
+    start_urls = [
+        '../html/catgut-papers.html',
+    ]
+
+    def parse(self, response):
+        for i, x in \
+            enumerate(esponse.css('div.c02 p b::text').extract()):
+            if x.find('Abbot'):
+                print(i)
+                break
+        #     yield {
+        #         'author': quote.xpath('span/small/text()').get(),
+        #         'text': quote.css('span.text::text').get(),
+        #     }
+
+        # next_page = response.css('li.next a::attr("href")').get()
+        # if next_page is not None:
+        #     yield response.follow(next_page, self.parse)
