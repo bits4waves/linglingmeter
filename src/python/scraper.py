4547fed19b18bf164af0668ff1f785803e0c21e5
Rafael Beirigo <rafaelbeirigo@gmail.com>
2021-07-03 08:54:30 -0300

    scrape also journal info

--- src/python/scraper.py
+++ src/python/scraper.py
@@ -16,9 +16,13 @@ class ArticlesSpider(scrapy.Spider):
     def parse(self, response):
         everything = response.css('div.c02 p b::text').extract()
         for i, x in enumerate(everything):
-            if i < 162: continue
-            if i % 2 == 0:
-                y = x
+            if i < 162:
+                # Journal entries.
+                yield {'i': i, 'x': cleanup(x)}
             else:
-                y = y + ' ' + x
-                yield {'i': i, 'y': cleanup(y)}
+                # Article entries.
+                if i % 2 == 0:
+                    y = x
+                else:
+                    y = y + ' ' + x
+                    yield {'i': i, 'y': cleanup(y)}
